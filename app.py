d0bca874638a0acd1ff3f19c2621fdb593fccde2
FelipeBarbosaPoeiras <felipepoeiras21@gmail.com>
2026-05-25 08:03:48 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, jsonify
+import math
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        usuario = request.form.get('usuario')
-        senha = request.form.get('senha')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-        # ⚠️ Altere aqui com o seu nome e sua matrícula da escola
-        NOME_CORRETO = 'Felipe'
-        MATRICULA_CORRETA = '12402320'
+@app.route('/calcular', methods=['POST'])
+def calcular():
 
-        # Validação dos dados digitados
-        if usuario.lower() == NOME_CORRETO.lower() and senha == MATRICULA_CORRETA:
-            return f"<h1>Acesso permitido! Bem-vindo(a), {usuario}!</h1>"
-        else:
-            # Se errar, recarrega a página enviando uma mensagem de erro
-            return render_template('login.html', erro="Nome ou Matrícula incorretos.")
-    
-    # Se for uma requisição GET, apenas mostra o formulário limpo
-    return render_template('login.html')
+    dados = request.get_json()
+    expressao = dados['expressao']
 
-if __name__ == "__main__":
+    try:
+
+        permitidos = {
+            "sqrt": math.sqrt,
+            "sin": math.sin,
+            "cos": math.cos,
+            "tan": math.tan,
+            "log": math.log10,
+            "ln": math.log,
+            "pi": math.pi,
+            "e": math.e,
+            "pow": pow
+        }
+
+        resultado = eval(expressao, {"__builtins__": None}, permitidos)
+
+        return jsonify({
+            "resultado": resultado
+        })
+
+    except Exception:
+        return jsonify({
+            "resultado": "Erro"
+        })
+
+if __name__ == '__main__':
     app.run(debug=True)
-
