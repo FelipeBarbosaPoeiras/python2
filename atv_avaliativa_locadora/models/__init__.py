029150fba45094db63ae10b1d5128f85a06d7070
FelipeBarbosaPoeiras <felipepoeiras21@gmail.com>
2026-06-22 18:00:46 -0300

    Add files via upload

---
+++ atv_avaliativa_locadora/models/__init__.py
@@ -0,0 +1,13 @@
+# Cenário: A - Locadora 
+# Aluno: Felipe Barbosa Poeiras
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+from .base import ModeloBase
+from .cliente_locadora import ClienteLocadora
+from .veiculo import Veiculo
+from .locacao import Locacao
+
+__all__ = ["db", "ModeloBase", "ClienteLocadora", "Veiculo", "Locacao"]
